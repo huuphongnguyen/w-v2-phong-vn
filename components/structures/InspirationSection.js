0c3211f7a1a974d7936e33eafb4523faf202d633
Huu Phong Nguyen <huuphong.nguyen1507@gmail.com>
2021-03-18 11:34:32 +0700

    Update InspirationSection.js

--- components/structures/InspirationSection.js
+++ components/structures/InspirationSection.js
@@ -18,7 +18,7 @@ export default function InspirationSection() {
       <div className="px-4 py-2">
         <NextLink href="/inspiration/archillect">
           <a>
-            <h2 className="text-black dark:text-white font-bold rainbow_text_hover">
+            <h2 className="text-black dark:text-white font-bold inline-block bg-yellow-400 rainbow_text_hover ">
               {t("inspiration-archillect-title")}
             </h2>
           </a>
@@ -31,7 +31,7 @@ export default function InspirationSection() {
       <div className="px-4 py-2">
         <NextLink href="/inspiration/twitter-hashflags">
           <a>
-            <h2 className="text-black dark:text-white font-bold inline-flex rainbow_text_hover ">
+            <h2 className="text-black dark:text-white font-bold inline-flex rainbow_text_hover">
               {t("inspiration-twitterhashflags-title")}
             </h2>
           </a>
