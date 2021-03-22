84619ba6d7987d461e178e1f76aa9248447238a7
Huu Phong Nguyen <huuphong.nguyen1507@gmail.com>
2021-03-22 17:28:27 +0700

    💠 Test new logo

--- components/elements/Logo.js
+++ components/elements/Logo.js
@@ -1,4 +1,5 @@
 import useTranslation from "next-translate/useTranslation";
+import NextImage from "next/image";
 
 export default function Logo() {
   const { t } = useTranslation("common");
@@ -6,7 +7,13 @@ export default function Logo() {
   return (
     <a href="https://phong.vn">
       <div className="inline-flex items-center space-x-2">
-        <div className="w-7 h-7 bg-black dark:bg-white"></div>
+        <div className="w-16 h-16 relative">
+          <NextImage
+            src="/phong-logo.gif"
+            className="object-cover absolute filter filter-saturate-175"
+            layout="fill"
+          />
+        </div>
         <h1 className="text-black dark:text-white font-carbon-bold uppercase text-xl rainbow_text_hover">
           {t("logo-title")}
         </h1>
--- tailwind.config.js
+++ tailwind.config.js
@@ -66,5 +66,6 @@ module.exports = {
   plugins: [
     require("@tailwindcss/aspect-ratio"),
     require("@tailwindcss/line-clamp"),
+    require("tailwindcss-css-filters"),
   ],
 };
