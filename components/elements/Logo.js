6494cfd19086b3d2f535c809f01797528778b3a8
Huu Phong Nguyen <huuphong.nguyen1507@gmail.com>
2021-05-13 16:07:32 +0700

    🧿 Update new logo

--- components/elements/Logo.js
+++ components/elements/Logo.js
@@ -1,5 +1,4 @@
 import useTranslation from "next-translate/useTranslation";
-import NextImage from "next/image";
 import LogoCoin from "./LogoCoin";
 
 export default function Logo() {
@@ -8,16 +7,9 @@ export default function Logo() {
   return (
     <a href="https://phong.vn">
       <div className="inline-flex items-center space-x-2">
-        <div className="w-16 h-16 relative">
-          <NextImage
-            src="/phong-logo.gif"
-            className="object-cover absolute filter filter-saturate-175"
-            layout="fill"
-          />
-        </div>
-        {/* <div>
+        <div className="-mt-20 transform translate-y-10 scale-110 origin-center">
           <LogoCoin />
-        </div> */}
+        </div>
         <h1 className="text-black dark:text-white font-carbon-bold uppercase text-xl rainbow_text_hover">
           {t("logo-title")}
         </h1>
