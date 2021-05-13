d598c34f52dedc5a8896ed128b497ffc573934ea
Huu Phong Nguyen <huuphong.nguyen1507@gmail.com>
2021-05-13 19:55:00 +0700

    Update VideoSection.js

--- components/structures/VideoSection.js
+++ components/structures/VideoSection.js
@@ -1,16 +1,32 @@
 import NextImage from "next/image";
 
 export default function VideoSection() {
+  const videolist = [
+    "https://www.youtube.com/embed/uWKlfC7N3Kw",
+    "https://www.youtube.com/embed/sX1Y2JMK6g8",
+    "https://www.youtube.com/embed/QJbpJQscn9E",
+    "https://www.youtube.com/embed/4u6bWs-ZG0o",
+    "https://www.youtube.com/embed/4PweEtDSqUs&t",
+    "https://www.youtube.com/embed/3AOfv_cNWok",
+    "https://www.youtube.com/embed/tQDvPwNBC90",
+    "https://www.youtube.com/embed/8FFF-QQeVds",
+    "https://www.youtube.com/embed/Vph0-ntRSRo",
+    "https://www.youtube.com/embed/F8MN0o6RS9o",
+    "https://www.youtube.com/embed/0xzDG8NLCxI",
+    "https://www.youtube.com/embed/BOa0zQBRs_M",
+  ];
+  const video = videolist[Math.floor(Math.random() * videolist.length)];
+
   return (
     <div className="space-y-4">
       <p className="text-black font-carbon-bold text-xl text-center">
-        A weird video is picked by someone on the Internet
+        A weird video is picked randomly
       </p>
       <div className="flex items-center justify-center">
         <div className="absolute z-20">
           <iframe
             className="w-[360px] md:w-[460px] h-[250px]"
-            src="https://www.youtube.com/embed/uWKlfC7N3Kw"
+            src={video}
             title="YouTube video player"
             frameborder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
