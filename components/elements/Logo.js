144988e0083ef9a51cf4cbeccc51c137667bbd93
Huu Phong Nguyen <huuphong.nguyen1507@gmail.com>
2021-05-13 16:12:59 +0700

    🧿 Update logo

--- components/elements/Logo.js
+++ components/elements/Logo.js
@@ -7,7 +7,7 @@ export default function Logo() {
   return (
     <a href="https://phong.vn">
       <div className="inline-flex items-center space-x-2">
-        <div className="-mt-20 transform translate-y-10 scale-110 origin-center">
+        <div className="-mt-20 transform translate-y-10 scale-110 origin-center mr-2">
           <LogoCoin />
         </div>
         <h1 className="text-black dark:text-white font-carbon-bold uppercase text-xl rainbow_text_hover">
