1d2cd342851cd66b3f88e050fe977f9d1efd7750
Huu Phong Nguyen <huuphong.nguyen1507@gmail.com>
2021-06-06 10:40:17 +0700

    🔫 Update Projects Section UI

--- components/structures/ProjectsSection.js
+++ components/structures/ProjectsSection.js
@@ -12,32 +12,11 @@ import {
 import useTranslation from "next-translate/useTranslation";
 import NextImage from "next/image";
 import { ArrowUpRight } from "akar-icons";
-import toast from "react-hot-toast";
 
 export default function ProjectsSection() {
   const { t } = useTranslation("common");
   return (
     <div>
-      <div
-        className="w-[326px] h-[480px] hidden md:block absolute transform scale-[0.55] origin-center -translate-x-16 -translate-y-16 -rotate-6 cursor-pointer transition-all hover:scale-[0.6] hover:rotate-6"
-        onClick={() =>
-          toast(
-            <p className="text-black dark:text-white text-base">
-              {t("common:projects-toast-image")}
-            </p>,
-            {
-              icon: "🤟",
-            }
-          )
-        }
-      >
-        <NextImage
-          src="https://media0.giphy.com/media/H6bpouTIgqRYn8xzyz/giphy.gif?cid=4fcb451e3i8ou5hf8je0vjdsrs8fue3t1pp0cxkxt27ye39i&rid=giphy.gif&ct=s"
-          layout="fill"
-          className="absolute"
-          quality={30}
-        />
-      </div>
       <div className="flex items-center space-x-2">
         <div className="inline-flex items-center space-x-1">
           <p className="text-black dark:text-white text-xl uppercase font-carbon-bold">
@@ -57,57 +36,90 @@ export default function ProjectsSection() {
         </a>
       </div>
 
-      <div className="px-4 py-2 space-y-4 pl-4 md:pl-[240px]">
-        <div>
-          <NextLink href="/projects/axembroidery">
-            <a>
-              <h2 className="text-black dark:text-white font-bold rainbow_text_hover inline-block">
-                {t("projects-ax-title")}
-              </h2>
-            </a>
-          </NextLink>
-          <p className="text-gray-500 dark:text-gray-400">
-            {t("projects-ax-description")}
-          </p>
-          <div className="flex space-x-2">
-            <Hashtag_2017 t={t} />
-            <Hashtag_developing t={t} />
+      <div className="px-4 py-2 space-y-4 pl-3">
+        <div className="flex items-start space-x-3">
+          <div className="w-[10%] md:w-auto pt-2">
+            <div className="w-8 h-8 relative">
+              <NextImage
+                src="https://dl.dropboxusercontent.com/s/h9qd7mgchwuspxb/axembroidery-feature-icon.svg?dl=0"
+                layout="fill"
+                className="object-cover absolute"
+              />
+            </div>
+          </div>
+          <div>
+            <NextLink href="/projects/axembroidery">
+              <a>
+                <h2 className="text-black dark:text-white font-bold rainbow_text_hover inline-block">
+                  {t("projects-ax-title")}
+                </h2>
+              </a>
+            </NextLink>
+            <p className="text-gray-500 dark:text-gray-400">
+              {t("projects-ax-description")}
+            </p>
+            <div className="flex space-x-2">
+              <Hashtag_2017 t={t} />
+              <Hashtag_developing t={t} />
+            </div>
           </div>
         </div>
 
-        <div>
-          <NextLink href="/projects/embvn">
-            <a>
-              <h2 className="text-black dark:text-white font-bold rainbow_text_hover inline-block">
-                {t("projects-embvn-title")}
-              </h2>
-            </a>
-          </NextLink>
-          <p className="text-gray-500 dark:text-gray-400">
-            {t("projects-embvn-description")}
-          </p>
-          <div className="flex space-x-2">
-            <Hashtag_2021 t={t} />
-            <Hashtag_developing t={t} />
+        <div className="flex items-start space-x-3">
+          <div className="w-[10%] md:w-auto pt-2">
+            <div className="w-8 h-8 relative">
+              <NextImage
+                src="https://dl.dropboxusercontent.com/s/98kw1mqm0cg0mmb/embvn-feature-icon.svg?dl=0"
+                layout="fill"
+                className="object-cover absolute"
+              />
+            </div>
+          </div>
+          <div>
+            <NextLink href="/projects/embvn">
+              <a>
+                <h2 className="text-black dark:text-white font-bold rainbow_text_hover inline-block">
+                  {t("projects-embvn-title")}
+                </h2>
+              </a>
+            </NextLink>
+            <p className="text-gray-500 dark:text-gray-400">
+              {t("projects-embvn-description")}
+            </p>
+            <div className="flex space-x-2">
+              <Hashtag_2021 t={t} />
+              <Hashtag_developing t={t} />
+            </div>
           </div>
         </div>
 
-        <div>
-          <NextLink href="/projects/blvcksaigon">
-            <a>
-              <h2 className="text-black dark:text-white font-bold rainbow_text_hover inline-block">
-                {t("projects-blvck-title")}
-              </h2>
-            </a>
-          </NextLink>
-          <p className="text-gray-500 dark:text-gray-400">
-            {t("projects-blvck-description")}
-          </p>
-          <div className="flex space-x-2">
-            <Hashtag_2019 t={t} />
-            <Hashtag_2020 t={t} />
-            <Hashtag_shutdown t={t} />
-            <Hashtag_stillloveit t={t} />
+        <div className="flex items-start space-x-3">
+          <div className="w-[10%] md:w-auto pt-2">
+            <div className="w-8 h-8 relative">
+              <NextImage
+                src="https://dl.dropboxusercontent.com/s/l56z44w3sk27jbr/blvcksaigon-feature-icon.svg?dl=0"
+                layout="fill"
+                className="object-cover absolute"
+              />
+            </div>
+          </div>
+          <div>
+            <NextLink href="/projects/blvcksaigon">
+              <a>
+                <h2 className="text-black dark:text-white font-bold rainbow_text_hover inline-block">
+                  {t("projects-blvck-title")}
+                </h2>
+              </a>
+            </NextLink>
+            <p className="text-gray-500 dark:text-gray-400">
+              {t("projects-blvck-description")}
+            </p>
+            <div className="flex space-x-2">
+              <Hashtag_2019 t={t} />
+              <Hashtag_2020 t={t} />
+              <Hashtag_shutdown t={t} />
+              <Hashtag_stillloveit t={t} />
+            </div>
           </div>
         </div>
       </div>
