1a67cf629136940f3de405711344ceff778293f5
Huu Phong Nguyen <huuphong.nguyen1507@gmail.com>
2021-06-07 10:48:01 +0700

    🧸 New stuff

--- pages/projects.js
+++ pages/projects.js
@@ -1,8 +1,24 @@
 import Container from "../components/Container";
 import NextLink from "next/link";
 import ProjectsElements from "../components/elements/ProjectsElement";
+import { Client } from "@notionhq/client";
 
-export default function Projects() {
+export async function getStaticProps() {
+  const notion = new Client({ auth: process.env.NOTION_API_OFFICIAL_KEYS });
+
+  const databaseId = process.env.NOTION_PAGE_ID_PROJECTS;
+  const response = await notion.databases.query({
+    database_id: databaseId,
+  });
+  return {
+    props: {
+      results: response.results,
+    },
+  };
+}
+
+export default function Projects({ results }) {
+  console.log("pr", results);
   return (
     <Container>
       <div className="items-start mx-auto w-full max-w-3xl space-y-4">
@@ -18,71 +34,22 @@ export default function Projects() {
           <h1>Projects</h1>
         </div>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-          <ProjectsElements
-            imagesbanner={axlistimg}
-            nameproject="AX Embroidery Workshop"
-            est="2017"
-            startupday="20171009"
-            projecturl="/projects/axembroidery"
-            qrimageurl="/static/projects-axembroidery-qr-code.png"
-          />
-          <ProjectsElements
-            imagesbanner={embvnlisting}
-            nameproject="EMB.VN - EMB Vietnam"
-            est="2021"
-            startupday="20210301"
-            projecturl="/projects/embvn"
-            qrimageurl="/static/projects-embvn-qr-code.png"
-          />
-          <ProjectsElements
-            imagesbanner={blvcksaigonlisting}
-            nameproject="BLVCK Saigon"
-            est="2019"
-            startupday="20191224"
-            projecturl="/projects/blvcksaigon"
-            qrimageurl="/static/projects-blvcksaigon-qr-code.png"
-          />
+          {results.map((block) => (
+            <div className={block.id}>
+              <ProjectsElements
+                imagesbanner={block.properties.imagesbanner.files.map(
+                  (image) => image.name
+                )}
+                nameproject={block.properties.nameproject.title[0].plain_text}
+                est={block.properties.est.rich_text[0].plain_text}
+                startupday={block.properties.startupday.rich_text[0].plain_text}
+                projecturl={block.properties.projecturl.rich_text[0].plain_text}
+                qrimageurl={block.properties.qrimageurl.rich_text[0].plain_text}
+              />
+            </div>
+          ))}
         </div>
       </div>
     </Container>
   );
 }
-
-export const axlistimg = [
-  "https://theuvitinhanhxuyen.files.wordpress.com/2020/08/theu-vi-tinh-tphcm-anh-xuyen-anh-khach-hang-img8-1.png?w=300",
-  "https://theuvitinhanhxuyen.files.wordpress.com/2020/08/theu-vi-tinh-tphcm-anh-xuyen-anh-khach-hang-img5.png?w=300",
-  "https://theuvitinhanhxuyen.files.wordpress.com/2020/08/theu-vi-tinh-tphcm-anh-xuyen-anh-khach-hang-img1.png?w=300",
-  "https://theuvitinhanhxuyen.files.wordpress.com/2020/08/theu-vi-tinh-tphcm-anh-xuyen-anh-khach-hang-img9-1.png?w=300",
-  "https://theuvitinhanhxuyen.files.wordpress.com/2020/08/theu-vi-tinh-tphcm-anh-xuyen-anh-khach-hang-img2.png?w=300",
-  "https://theuvitinhanhxuyen.files.wordpress.com/2021/04/0a721ce2-1456-4b2d-8da2-62706337cf91_1_105_c.jpeg",
-  "https://theuvitinhanhxuyen.files.wordpress.com/2020/08/theu-vi-tinh-tphcm-anh-xuyen-anh-khach-hang-img3.png?w=300",
-  "https://theuvitinhanhxuyen.files.wordpress.com/2020/08/theu-vi-tinh-tphcm-anh-xuyen-anh-khach-hang-img4.png?w=300",
-  "https://theuvitinhanhxuyen.files.wordpress.com/2020/08/theu-vi-tinh-tphcm-anh-xuyen-anh-khach-hang-img12-1.jpeg?w=300",
-  "https://theuvitinhanhxuyen.files.wordpress.com/2020/08/theu-vi-tinh-tphcm-anh-xuyen-anh-khach-hang-img12.png?w=300",
-];
-
-export const blvcksaigonlisting = [
-  "https://dl.dropboxusercontent.com/s/1nt2nged54p3105/phong-vn-project-blvcksaigon-img1.png?dl=0",
-  "https://dl.dropboxusercontent.com/s/lz2jjjdyu8y7tln/phong-vn-project-blvcksaigon-img2.png?dl=0",
-  "https://dl.dropboxusercontent.com/s/j3nyeora0t0ul3f/phong-vn-project-blvcksaigon-img3.png?dl=0",
-  "https://dl.dropboxusercontent.com/s/0ehsesfdikhi6qe/phong-vn-project-blvcksaigon-img4.png?dl=0",
-  "https://dl.dropboxusercontent.com/s/zojf6uj8kusrrvi/phong-vn-project-blvcksaigon-img5.png?dl=0",
-  "https://dl.dropboxusercontent.com/s/eya4tyyg6ympbg9/phong-vn-project-blvcksaigon-img6.png?dl=0",
-  "https://dl.dropboxusercontent.com/s/47x4ujvie6cr4dk/phong-vn-project-blvcksaigon-img7.png?dl=0",
-  "https://dl.dropboxusercontent.com/s/51jdq25khqq835k/phong-vn-project-blvcksaigon-img8.png?dl=0",
-  "https://dl.dropboxusercontent.com/s/26uhjnfvunchurh/phong-vn-project-blvcksaigon-img9.png?dl=0",
-  "https://dl.dropboxusercontent.com/s/csclwmxzzx3ft2g/phong-vn-project-blvcksaigon-img10.png?dl=0",
-];
-
-export const embvnlisting = [
-  "https://dl.dropboxusercontent.com/s/2842xhc0o9caype/phong-vn-project-embvn-img1.png?dl=0",
-  "https://dl.dropboxusercontent.com/s/nd66et46paxibre/phong-vn-project-embvn-img2.png?dl=0",
-  "https://dl.dropboxusercontent.com/s/hpb2bd2vqcoj1aa/phong-vn-project-embvn-img3.png?dl=0",
-  "https://dl.dropboxusercontent.com/s/mdbwvcz80oyxtem/phong-vn-project-embvn-img4.png?dl=0",
-  "https://dl.dropboxusercontent.com/s/f7mttvrrx0zm00h/phong-vn-project-embvn-img5.png?dl=0",
-  "https://dl.dropboxusercontent.com/s/ylt8041ufw6j7hs/phong-vn-project-embvn-img6.png?dl=0",
-  "https://dl.dropboxusercontent.com/s/j72nk9gnpmqu1ge/phong-vn-project-embvn-img7.png?dl=0",
-  "https://dl.dropboxusercontent.com/s/2rp6izixhiz34a9/phong-vn-project-embvn-img8.png?dl=0",
-  "https://dl.dropboxusercontent.com/s/d3dnuvitd0adw2f/phong-vn-project-embvn-img9.png?dl=0",
-  "https://dl.dropboxusercontent.com/s/3godo1teagrivtm/phong-vn-project-embvn-img10.png?dl=0",
-];
--- pages/projects/axembroidery.js
+++ pages/projects/axembroidery.js
@@ -50,6 +50,13 @@ export default function AXEmbroidery() {
         description={seodescrip}
       />
       <div className="items-start mx-auto w-full max-w-3xl space-x-2 space-y-4">
+        <NextLink href="/">
+          <a>
+            <div className="inline-flex items-center space-x-1 text-gray-400 dark:text-gray-500 hover:text-black dark:hover:text-white text-xl uppercase font-carbon-bold">
+              <p>Front-Page</p>
+            </div>
+          </a>
+        </NextLink>
         <NextLink href="/projects">
           <a>
             <div className="inline-flex items-center space-x-1 text-gray-400 dark:text-gray-500 hover:text-black dark:hover:text-white text-xl uppercase font-carbon-bold">
